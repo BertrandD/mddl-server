91a92d096d281a29c55930c2e29fe3fff3962808
Philippe Leboc <LebocPhilippe@live.be>
2016-04-10 18:40:00 +0200

    feat(config): ServerPort configurable

--- src/com/Application.java
+++ src/com/Application.java
@@ -3,8 +3,6 @@ package com;
 import com.config.Config;
 import com.gameserver.data.xml.impl.BuildingData;
 import com.gameserver.data.xml.impl.ItemData;
-import com.gameserver.model.buildings.Storage;
-import com.gameserver.model.commons.Requirement;
 import com.util.Utils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,9 +11,20 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
     public static void main(String[] args)
     {
-        SpringApplication.run(Application.class, args);
-        Config.load();
+        String MODE = Config.DEV_CONFIG_DIRECTORY;
+
+        if(args.length > 0){
+            if(args[0].equalsIgnoreCase("prod")){
+                MODE = Config.PROD_CONFIG_DIRECTORY;
+            }
+        }
+
+        Config.load(MODE);
         BuildingData.getInstance();
         ItemData.getInstance();
+
+        Utils.println("====================================================");
+        System.setProperty("server.port", ""+Config.SERVER_PORT);
+        SpringApplication.run(Application.class, args);
     }
 }
