edc6d8049953cf9741d98f19aeeef56067c81fb8
Bertrand Darbon <bertrand.darbon@gmail.com>
2016-04-18 00:08:14 +0200

    fix(building): make /building/create useable

--- src/com/gameserver/controllers/BuildingInstanceController.java
+++ src/com/gameserver/controllers/BuildingInstanceController.java
@@ -1,12 +1,18 @@
 package com.gameserver.controllers;
 
+import com.auth.Account;
 import com.fasterxml.jackson.annotation.JsonView;
 import com.gameserver.model.Base;
+import com.gameserver.model.Player;
 import com.gameserver.model.instances.BuildingInstance;
 import com.gameserver.services.BaseService;
 import com.gameserver.services.BuildingService;
+import com.gameserver.services.PlayerService;
+import com.util.data.json.Response.JsonResponse;
+import com.util.data.json.Response.JsonResponseType;
 import com.util.data.json.View;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -28,6 +34,9 @@ public class BuildingInstanceController {
     @Autowired
     private BaseService baseService;
 
+    @Autowired
+    private PlayerService playerService;
+
     @JsonView(View.Standard.class)
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public Collection<BuildingInstance> findAll(){
@@ -42,19 +51,20 @@ public class BuildingInstanceController {
 
     @JsonView(View.buildingInstance_base.class)
     @RequestMapping(method = RequestMethod.POST)
-    public BuildingInstance create(@RequestParam(value = "base") String baseId, @RequestParam(value = "building") String templateId){
-        Base base = baseService.findOne(baseId);
-        if(base == null) return null;
+    public JsonResponse create(@AuthenticationPrincipal Account pAccount, @RequestParam(value = "building") String templateId){
+        Player currentPlayer = playerService.findOne(pAccount.getCurrentPlayer());
+        Base base = currentPlayer.getCurrentBase();
+        if(base == null) return new JsonResponse(JsonResponseType.ERROR, "Player base not found !");
 
         BuildingInstance building = buildingService.create(base, templateId);
-        if(building == null) return null;
+        if(building == null) return new JsonResponse(JsonResponseType.ERROR, "Can not create base !");
 
         base.addBuilding(building);
         baseService.update(base);
 
         // TODO
 
-        return building;
+        return new JsonResponse(building);
     }
 
     @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
