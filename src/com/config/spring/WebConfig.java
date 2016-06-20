d0d1dd80833c659e1a1ab0672fa8cf6532d45e81
Philippe Leboc <LebocPhilippe@live.be>
2016-06-21 00:26:37 +0200

    chore(spring): centralized configuration

--- src/com/config/spring/WebConfig.java
+++ src/com/config/spring/WebConfig.java
@@ -1,8 +1,6 @@
 package com.config.spring;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -11,8 +9,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
  * @author Bertrand
  */
 @Configuration
-@EnableScheduling
-@EnableAsync
 @EnableWebMvc
 public class WebConfig extends WebMvcConfigurerAdapter {
 
