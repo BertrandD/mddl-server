071804482dbb5270ebc29a60c5e32be37f206810
Philippe Leboc <LebocPhilippe@live.be>
2016-06-25 20:43:23 +0200

    Allow synchronize mongodb with last commit

--- src/com/gameserver/services/UpdateService.java
+++ src/com/gameserver/services/UpdateService.java
@@ -1,7 +1,11 @@
 package com.gameserver.services;
 
+import com.gameserver.model.instances.ItemInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoOperations;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.stereotype.Service;
 
 /**
@@ -18,5 +22,9 @@ public class UpdateService {
      */
     public void updateDatabase() {
         // All update will be writed here.
+        Update update = new Update();
+        update.unset("lastRefresh");
+
+        mongoOperations.findAndModify(new Query(Criteria.where("lastRefresh").exists(true)), update, ItemInstance.class);
     }
 }
