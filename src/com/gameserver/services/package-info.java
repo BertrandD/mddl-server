d0b5ad87a3afc6c0ad72503efcf1893c4d7f0628
Philippe Leboc <LebocPhilippe@live.be>
2016-06-29 16:05:37 +0200

    remove lots of old work

--- src/com/gameserver/services/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * @author LEBOC Philippe
- *
- * This package contains all Service wich manage Objects from (com.gameserver.model.*) that interract with mongoDB.
- */
-package com.gameserver.services;
