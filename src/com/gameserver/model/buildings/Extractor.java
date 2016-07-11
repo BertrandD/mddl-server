8253d853618944e6c0385f5342ab9942bb162968
Philippe Leboc <LebocPhilippe@live.be>
2016-07-11 16:27:48 +0200

    fix(Building): remove unused property

--- src/com/gameserver/model/buildings/Extractor.java
+++ src/com/gameserver/model/buildings/Extractor.java
@@ -1,26 +1,12 @@
 package com.gameserver.model.buildings;
 
 import com.gameserver.model.commons.StatsSet;
-import com.gameserver.model.items.GameItem;
-
-import java.util.List;
 
 /**
  * @author LEBOC Philippe
  */
 public class Extractor extends ModulableBuilding {
-
-    private List<GameItem> produceItems;
-
     public Extractor(StatsSet set) {
         super(set);
-    }
-
-    public void setProduceItems(List<GameItem> produceItems) {
-        this.produceItems = produceItems;
-    }
-
-    public List<GameItem> getProduceItems() {
-        return produceItems;
     }
 }
