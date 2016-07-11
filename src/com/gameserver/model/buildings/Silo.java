5cfe2395ff970a67191390e30f3b2cc29cc529ab
Philippe Leboc <LebocPhilippe@live.be>
2016-07-11 21:00:40 +0200

    fix(Silo): removed wrong way to select stat

--- src/com/gameserver/model/buildings/Silo.java
+++ src/com/gameserver/model/buildings/Silo.java
@@ -1,12 +1,6 @@
 package com.gameserver.model.buildings;
 
-import com.gameserver.enums.StatOp;
-import com.gameserver.holders.StatHolder;
 import com.gameserver.model.commons.StatsSet;
-import com.gameserver.model.stats.BaseStat;
-
-import java.util.Collections;
-import java.util.List;
 
 /**
  * @author LEBOC Philippe
@@ -17,9 +11,5 @@ public class Silo extends ModulableBuilding {
         super(set);
     }
 
-    @Override
-    public List<StatHolder> getStats() {
-        if(getModules().size() != 1) return Collections.singletonList(new StatHolder(BaseStat.NONE, StatOp.NONE));
-        return Collections.singletonList(super.getStat(getModules().get(0).getUnlockStat()));
-    }
+
 }
