c5be4d9bd5b64c8c6ca461d813581f7ffae8f526
Philippe Leboc <LebocPhilippe@live.be>
2016-07-13 15:18:56 +0200

    feat(Controller): add ShipController

--- src/com/gameserver/controllers/ship/ShipController.java
+++ src/com/gameserver/controllers/ship/ShipController.java
@@ -4,8 +4,13 @@ import com.auth.Account;
 import com.gameserver.data.xml.impl.ItemData;
 import com.gameserver.model.Base;
 import com.gameserver.model.Player;
+import com.gameserver.model.instances.ItemInstance;
+import com.gameserver.model.inventory.BaseInventory;
+import com.gameserver.model.items.GameItem;
 import com.gameserver.model.vehicles.Ship;
+import com.gameserver.services.InventoryService;
 import com.gameserver.services.PlayerService;
+import com.gameserver.services.ShipService;
 import com.util.response.JsonResponse;
 import com.util.response.JsonResponseType;
 import com.util.response.SystemMessageId;
@@ -17,8 +22,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 /**
  * @author LEBOC Philippe
@@ -31,8 +36,15 @@ public class ShipController {
     @Autowired
     private PlayerService playerService;
 
+    @Autowired
+    private InventoryService inventoryService;
+
+    @Autowired
+    private ShipService shipService;
+
     @RequestMapping(method = RequestMethod.POST)
     public JsonResponse create(@AuthenticationPrincipal Account pAccount,
+                               @RequestParam(value = "count") long count,
                                @RequestParam(value = "structureId") String structure,
                                @RequestParam(value = "attachments") List<String> ids) {
 
@@ -45,20 +57,29 @@ public class ShipController {
 
         if(ItemData.getInstance().getStructure(structure) == null) return new JsonResponse(JsonResponseType.ERROR, SystemMessageId.ITEM_NOT_FOUND);
 
+        final BaseInventory inventory = base.getBaseInventory();
+        final List<ItemInstance> collector = new ArrayList<>();
+
+        final ItemInstance structuresInst = inventory.getItems().stream().filter(i->i.getTemplateId().equals(structure)).findFirst().orElse(null);
+        if(structuresInst == null || structuresInst.getCount() < count) return new JsonResponse(JsonResponseType.ERROR, SystemMessageId.ITEM_NOT_FOUND); // todo: make a new sysmsg
+
         boolean faillure = false;
-        for (int i = 0; i < ids.size() && !faillure; i++)
-            if(ItemData.getInstance().getTemplate(ids.get(i)) == null) faillure = true;
+        for (int i = 0; i < ids.size() && !faillure; i++){
+            final GameItem template = ItemData.getInstance().getTemplate(ids.get(i));
+            if(template == null) faillure = true; else {
+                final ItemInstance inst = inventory.getItems().stream().filter(item -> item.getTemplateId().equals(template.getItemId())).findFirst().orElse(null);
+                if(inst != null && inst.getCount() >= count) collector.add(inst); else faillure = true;
+            }
+        }
 
         if(faillure) return new JsonResponse(JsonResponseType.ERROR, SystemMessageId.ITEM_NOT_FOUND);
 
-        // TODO: check if base has items.
+        for (ItemInstance inst : collector)
+            inventoryService.consumeItem(inst, 1);
 
-        final Ship ship = new Ship(base, structure, 1);
-        ship.setCargoIds(ids.stream().filter(k -> k.startsWith("cargo_")).collect(Collectors.toList()));
-        ship.setEngineIds(ids.stream().filter(k -> k.startsWith("engine_")).collect(Collectors.toList()));
-        ship.setModuleIds(ids.stream().filter(k -> k.startsWith("module_")).collect(Collectors.toList()));
-        ship.setWeaponIds(ids.stream().filter(k -> k.startsWith("weapon_")).collect(Collectors.toList()));
-        return new JsonResponse(JsonResponseType.ERROR);
+        final Ship ship = shipService.create(base, structure, count, ids);
+        if(ship == null) return new JsonResponse(JsonResponseType.ERROR, "Cannot create Ship");
+        return new JsonResponse(ship);
     }
 
 }
--- src/com/gameserver/services/ShipService.java
+++ src/com/gameserver/services/ShipService.java
@@ -1,7 +1,11 @@
 package com.gameserver.services;
 
+import com.gameserver.model.Base;
 import com.gameserver.model.vehicles.Ship;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * @author LEBOC Philippe
@@ -15,8 +19,22 @@ public class ShipService extends DatabaseService<Ship>{
 
     @Override
     public Ship create(Object... params) {
-        if(params.length != 1) return null;
-        final Ship ship = (Ship) params[0];
+        if(params.length < 3) return null;
+        final Base base = (Base) params[0];
+        final String structure = (String) params[1];
+        final long count = (long) params[2];
+        List<String> ids = null;
+
+        if(params.length > 3)
+            ids = (List<String>) params[3];
+
+        final Ship ship = new Ship(base, structure, count);
+        if(ids != null) {
+            ship.setCargoIds(ids.stream().filter(k -> k.startsWith("cargo_")).collect(Collectors.toList()));
+            ship.setEngineIds(ids.stream().filter(k -> k.startsWith("engine_")).collect(Collectors.toList()));
+            ship.setModuleIds(ids.stream().filter(k -> k.startsWith("module_")).collect(Collectors.toList()));
+            ship.setWeaponIds(ids.stream().filter(k -> k.startsWith("weapon_")).collect(Collectors.toList()));
+        }
         mongoOperations.insert(ship);
         return ship;
     }
