9880649713ae784ec01fb3795b82773d52c8dee5
Bertrand Darbon <bertrand.darbon@gmail.com>
2017-06-30 12:01:45 +0200

    add some PlayerManager tests

--- api/src/test/java/com/middlewar/tests/manager/PlayerManagerTest.java
+++ api/src/test/java/com/middlewar/tests/manager/PlayerManagerTest.java
@@ -1,11 +1,10 @@
 package com.middlewar.tests.manager;
 
 import com.middlewar.api.Application;
-import com.middlewar.api.exceptions.BaseNotFoundException;
-import com.middlewar.api.exceptions.NoPlayerConnectedException;
-import com.middlewar.api.exceptions.PlayerNotFoundException;
+import com.middlewar.api.exceptions.*;
 import com.middlewar.api.manager.PlayerManager;
 import com.middlewar.api.services.PlayerService;
+import com.middlewar.core.config.Config;
 import com.middlewar.core.model.Account;
 import com.middlewar.core.model.Player;
 import org.assertj.core.api.Assertions;
@@ -42,6 +41,7 @@ public class PlayerManagerTest {
 
     @Before
     public void init() throws NoPlayerConnectedException, PlayerNotFoundException {
+        Config.MAX_PLAYER_IN_ACCOUNT = 100;
         _player = new Player();
         _player.setId("yoloo");
         _player.setName("yoloo");
@@ -69,5 +69,38 @@ public class PlayerManagerTest {
     public void shouldThrowExceptionIfNoPlayerNotFound() throws NoPlayerConnectedException, PlayerNotFoundException {
         when(playerService.findOne("yoloo")).thenReturn(null);
         final Player player = playerManager.getCurrentPlayerForAccount(_account);
+    }
+
+    @Test(expected = MaxPlayerCreationReachedException.class)
+    public void shouldCheckMaxPlayer() throws MaxPlayerCreationReachedException, ForbiddenNameException, PlayerCreationFailedException, UsernameAlreadyExistsException {
+        Config.MAX_PLAYER_IN_ACCOUNT = 0;
+        playerManager.createForAccount(_account, "toto");
+    }
+
+    @Test(expected = UsernameAlreadyExistsException.class)
+    public void shouldCheckExistingUsername() throws MaxPlayerCreationReachedException, ForbiddenNameException, PlayerCreationFailedException, UsernameAlreadyExistsException {
+        when(playerService.findByName("toto")).thenReturn(_player);
+        playerManager.createForAccount(_account, "toto");
+    }
+
+    @Test(expected = ForbiddenNameException.class)
+    public void shouldCheckForbiddentNames() throws MaxPlayerCreationReachedException, ForbiddenNameException, PlayerCreationFailedException, UsernameAlreadyExistsException {
+        Config.FORBIDDEN_NAMES = "ToTo,truc".split(",");
+        playerManager.createForAccount(_account, "toto");
+    }
+
+    @Test(expected = PlayerCreationFailedException.class)
+    public void shouldThrowExceptionIfFailed() throws MaxPlayerCreationReachedException, ForbiddenNameException, PlayerCreationFailedException, UsernameAlreadyExistsException {
+        when( playerService.create(_account, "toto")).thenReturn(null);
+        playerManager.createForAccount(_account, "toto");
+    }
+
+    @Test
+    public void shouldReturnCreatedPlayer() throws MaxPlayerCreationReachedException, ForbiddenNameException, PlayerCreationFailedException, UsernameAlreadyExistsException {
+        final Player player2 = new Player(_account, "toto");
+        when( playerService.create(_account, "toto")).thenReturn(player2);
+        final Player player1 = playerManager.createForAccount(_account, "toto");
+        Assertions.assertThat(player1).isNotNull();
+        Assertions.assertThat(player2).isEqualTo(player1);
     }
 }
