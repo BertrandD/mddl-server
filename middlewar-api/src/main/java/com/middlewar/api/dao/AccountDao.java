35e17bcdabd51ba4084419b3e833bfc861c65676
Bertrand Darbon <bertrand.darbon@gmail.com>
2017-09-26 15:28:40 +0200

    (db) remove daos

--- middlewar-api/src/main/java/com/middlewar/api/dao/AccountDao.java
+++ middlewar-api/src/main/java/com/middlewar/api/dao/AccountDao.java
@@ -1,13 +1,65 @@
 package com.middlewar.api.dao;
 
 import com.middlewar.core.model.Account;
-import org.springframework.data.jpa.repository.JpaRepository;
+import com.middlewar.core.model.Player;
+import org.springframework.stereotype.Service;
 
-/**
- * @author LEBOC Philippe
- */
-public interface AccountDao extends JpaRepository<Account, Long> {
-    Account findByUsername(String username);
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Vector;
 
-    Account findByToken(String token);
+@Service
+public class AccountDAO implements DAO<Account> {
+    private Vector<Account> accounts = new Vector<>();
+    private Map<String, Account> accountsUsernames = new HashMap<>();
+    private Map<String, Account> accountsToken = new HashMap<>();
+
+    @Override
+    public void add(Account o) {
+        accounts.add(o);
+        accountsUsernames.put(o.getUsername(), o);
+        accountsToken.put(o.getToken(), o);
+    }
+
+    @Override
+    public void remove(Account o) {
+        accounts.remove(o);
+        accountsUsernames.remove(o.getUsername());
+        accountsToken.remove(o.getToken());
+    }
+
+    @Override
+    public List<Account> getAll() {
+        return accounts;
+    }
+
+    @Override
+    public int count() {
+        return accounts.size();
+    }
+
+    @Override
+    public Account getById(int i) {
+        try {
+            return accounts.get(i - 1);
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    public Account getUserFromToken(String token) {
+        return accountsToken.get(token);
+    }
+
+    public Account findByUsername(String username) {
+        return accountsUsernames.get(username);
+    }
+
+    @Override
+    public void deleteAll() {
+        accounts.clear();
+        accountsUsernames.clear();
+        accountsToken.clear();
+    }
 }
--- middlewar-api/src/main/java/com/middlewar/api/dao/BaseDao.java
+++ middlewar-api/src/main/java/com/middlewar/api/dao/BaseDao.java
@@ -1,10 +1,49 @@
 package com.middlewar.api.dao;
 
 import com.middlewar.core.model.Base;
-import org.springframework.data.jpa.repository.JpaRepository;
+import com.middlewar.core.model.Player;
+import org.springframework.stereotype.Service;
 
-/**
- * @author Leboc Philippe.
- */
-public interface BaseDao extends JpaRepository<Base, Long> {
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Vector;
+
+@Service
+public class BaseDAO implements DAO<Base> {
+    private Vector<Base> bases = new Vector<>();
+
+    @Override
+    public void add(Base o) {
+        bases.add(o);
+    }
+
+    @Override
+    public void remove(Base o) {
+        bases.remove(o);
+    }
+
+    @Override
+    public Vector<Base> getAll() {
+        return bases;
+    }
+
+    @Override
+    public int count() {
+        return bases.size();
+    }
+
+    @Override
+    public Base getById(int i) {
+        try {
+            return bases.get(i-1);
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    @Override
+    public void deleteAll() {
+        bases.clear();
+    }
 }
