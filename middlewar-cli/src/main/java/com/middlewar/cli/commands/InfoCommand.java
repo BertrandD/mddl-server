4fd59a1802a7bd2f14789adbdac34e60eda01d50
Bertrand Darbon <bertrand.darbon@gmail.com>
2017-09-27 18:16:10 +0200

    (cli) display more info for base

--- middlewar-cli/src/main/java/com/middlewar/cli/commands/InfoCommand.java
+++ middlewar-cli/src/main/java/com/middlewar/cli/commands/InfoCommand.java
@@ -2,6 +2,7 @@ package com.middlewar.cli.commands;
 
 import com.middlewar.cli.GameContext;
 import com.middlewar.client.BaseClient;
+import com.middlewar.client.PlayerClient;
 import com.middlewar.dto.AccountDTO;
 import com.middlewar.dto.BaseDTO;
 import com.middlewar.dto.PlayerDTO;
@@ -14,7 +15,7 @@ import java.util.List;
  */
 public class InfoCommand extends Command {
     public InfoCommand() {
-        super("info <account|player>", "Display info on the account");
+        super("info <account|player|base>", "Display info on the account");
     }
 
     @Override
@@ -38,6 +39,7 @@ public class InfoCommand extends Command {
                 System.out.println("Username : " + account.getUsername());
                 break;
             case "player":
+                GameContext.getInstance().setPlayer(PlayerClient.getPlayer(GameContext.getInstance().getPlayer().getId()));
                 PlayerDTO player = GameContext.getInstance().getPlayer();
                 if (player == null) {
                     System.out.println("You don't have any player. Let's create it !");
@@ -48,6 +50,7 @@ public class InfoCommand extends Command {
                 System.out.println("Name : " + player.getName());
                 break;
             case "base":
+                GameContext.getInstance().setBase(BaseClient.getBase(GameContext.getInstance().getBase().getId()));
                 BaseDTO base = GameContext.getInstance().getBase();
                 if (base == null) {
                     System.out.println("You don't have any base. Let's create it !");
@@ -56,6 +59,16 @@ public class InfoCommand extends Command {
                 System.out.println(base.toString());
                 System.out.println("ID   : " + base.getId());
                 System.out.println("Name : " + base.getName());
+                System.out.println("Resources : ");
+                base.getResources().forEach(k-> System.out.println(k.getCount() + "/" + k.getAvailableCapacity() + " (" + k.getProdPerHour() + "/h)"));
+                System.out.println("Buildings : ");
+                base.getBuildings().forEach(k-> {
+                    System.out.println("\t" + k.getBuildingId() + " (lvl " + k.getCurrentLevel() + ")");
+                    if (k.getEndsAt() != -1) {
+                        System.out.println("\t\t Upgrading to lvl " + (k.getCurrentLevel() + 1) + ". Time left : " + (k.getEndsAt() - System.currentTimeMillis()) );
+                    }
+                    k.getModules().forEach(m-> System.out.println("\t\t" + m));
+                });
                 break;
             case "base.buildable":
                 List<BuildingHolderDTO> buildings = BaseClient.getBuildables(GameContext.getInstance().getBase());
